daf1a02a150f553e1ea77f37298f1e4452fa7a5f
Aman Verma <aman.angular13@gmail.com>
2026-08-10 16:57:41 +0530

    initial project setup

---
+++ src/main/java/com/org/flight_booking_assistant/FlightBookingAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.org.flight_booking_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/flight_booking_assistant/FlightBookingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.flight_booking_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
